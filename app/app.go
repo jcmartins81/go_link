518d216de354282e04ccd7789614882cd4ff48fd
José C. Martins <zemartins81@gmail.com>
2022-03-22 19:19:05 -0300

    chore: add buscarIPs

--- app/app.go
+++ app/app.go
@@ -1,12 +1,46 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IP's e nomes de servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IP's por endereço",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
 
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
